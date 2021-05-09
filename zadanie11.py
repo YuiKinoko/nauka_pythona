d6a61e148dd90e83956fbe3b2776867e6b4adf05
YuiKinoko <agata.k.grzybowska@gmail.com>
2021-05-09 12:44:11 +0000

    zadanie11

---
+++ zadanie11.py
@@ -0,0 +1,13 @@
+#samochody = ['syrena', 'poloneza']
+#ilosc = [3, 5]
+
+#for idx in range( len(samochody)):
+    #print("idx:{0} {1}" .format(idx, samochody[idx]))
+    #print(samochody[idx] + " ma ilosc drzwi " + str(ilosc[idx]))
+
+azja = ['Japonia', 'Korea Pld']
+wyspy = [6800, 3000]
+
+for idx in range( len(azja)):
+    print("idx:{0} {1}" .format(idx, azja[idx]))
+    print(azja[idx] + " ma wysp - " + str(wyspy[idx]))
